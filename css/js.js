4a7b60929e94391cde92b27668a3a76e5b5f6721
StephenOnyekachi <88212958+StephenOnyekachi@users.noreply.github.com>
2022-08-23 20:44:53 +0100

    initial commit

---
+++ css/js.js
@@ -0,0 +1,27 @@
+
+const texts = ['Website Developer', 'Graghic Designer', 'Business Promoter', 'Entertainner'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if (count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+}());
+
+// .type{
+//     position: relative;
+//     top: 100px;
+// }
